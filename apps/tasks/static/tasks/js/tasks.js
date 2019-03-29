c59ba24391540e093dbe3b599b814d3a170a91b7
Jordan Dunbrack <jordan.dunbrack@gmail.com>
2019-03-29 11:25:51 -0700

    adding functionality

--- apps/tasks/static/tasks/js/tasks.js
+++ apps/tasks/static/tasks/js/tasks.js
@@ -1,55 +1,6 @@
 // remember on ui-droppable-hover to increase height and/or width slightly.
 // jquery .addClass will animate.
 
-$('#add-task').click(function(e) {
-    e.preventDefault();
-    let data = $('#new-task').serialize();
-    $.ajax({
-        type: "post",
-        url: "/tasks/create",
-        data: data,
-        success: function (response) {
-            $('.main-tile .task-list').append(response)
-            $('#close-modal').trigger('click');
-        }
-    });
-});
-
-$('.task-list').sortable({
-    cursor: 'move',
-    items: '> li',
-    opacity: 0.5,
-    revert: true,
-    tolerance: 'intersect',
-    zIndex: 1002,
-    connectWith: '.task-list',
-    placeholder: "task-placeholder",
-    forcePlaceholderSize: true,
-    activate: function () {
-        $('.task-list').addClass('border');
-    },
-    deactivate: function () {
-        $('.task-list').removeClass('border');
-    }
-});
-
-$('.task-list').on('sortreceive', function (e, ui) {
-    let data = "csrfmiddlewaretoken=" + document.getElementsByName('csrfmiddlewaretoken')[0].value + "&task-id=" + ui.item.attr('data-task-id') + "&receiver=" + $(this).parent('.task-tile').attr('data-user-id');
-    $.post({
-        url: "/tasks/assign",
-        data: data
-    });
-});
-
-$('a[href="/tasks/finish"]').click(function(e) {
-    e.preventDefault();
-    let data = "csrfmiddlewaretoken=" + document.getElementsByName('csrfmiddlewaretoken')[0].value + "&task-id=5";
-    $.post({
-        url: "/tasks/finish",
-        data: data
-    });
-    $(this).parent('.task-row').remove();
-});
 
 $(document).ready(function () {
     $(".info-popup").tooltip({
@@ -68,6 +19,57 @@ $(document).ready(function () {
             });
         }
     });
+
+    $('#add-task').click(function (e) {
+        e.preventDefault();
+        let data = $('#new-task').serialize();
+        $.ajax({
+            type: "post",
+            url: "/tasks/create",
+            data: data,
+            success: function (response) {
+                $('.main-tile .task-list').append(response)
+                $('#close-modal').trigger('click');
+            }
+        });
+    });
+
+    $('.task-list').sortable({
+        cursor: 'move',
+        items: '> li',
+        opacity: 0.5,
+        revert: true,
+        tolerance: 'intersect',
+        zIndex: 1002,
+        connectWith: '.task-list',
+        placeholder: "task-placeholder",
+        forcePlaceholderSize: true,
+        activate: function () {
+            $('.task-list').addClass('border');
+        },
+        deactivate: function () {
+            $('.task-list').removeClass('border');
+        }
+    });
+
+    $('.task-list').on('sortreceive', function (e, ui) {
+        let data = "csrfmiddlewaretoken=" + document.getElementsByName('csrfmiddlewaretoken')[0].value + "&task-id=" + ui.item.attr('data-task-id') + "&receiver=" + $(this).parent('.task-tile').attr('data-user-id');
+        $.post({
+            url: "/tasks/assign",
+            data: data
+        });
+    });
+
+    $('a[href="/tasks/finish"]').click(function (e) {
+        e.preventDefault();
+        let data = "csrfmiddlewaretoken=" + document.getElementsByName('csrfmiddlewaretoken')[0].value + "&task-id=5";
+        $.post({
+            url: "/tasks/finish",
+            data: data
+        });
+        $(this).parent('.task-row').remove();
+    });
+
 
     $(".info-popup").mouseout(function () {
         // re-initializing tooltip
