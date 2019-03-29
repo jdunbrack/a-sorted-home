487614baffda05fd789443f0374d5df25c6464bd
Jordan Dunbrack <jordan.dunbrack@gmail.com>
2019-03-28 17:12:15 -0700

    add task app

--- apps/tasks/static/tasks/js/tasks.js
+++ apps/tasks/static/tasks/js/tasks.js
@@ -1,25 +1,25 @@
 // remember on ui-droppable-hover to increase height and/or width slightly.
 // jquery .addClass will animate.
 
-$('#add-task').click(function(e) {
-    e.preventDefault();
-    let data = $('#new-task').serialize();
-    $.ajax({
-        type: "post",
-        url: "/tasks/create",
-        data: data,
-        success: function (response) {
-            $('.main-tile').append(response)
-            $('.task-row').draggable({
-                cursor: 'move',
-                opacity: .7,
-                revert: 'invalid',
-                scroll: true,
-                scrollSensitivity: 50,
-            });
-        }
-    });
-});
+// $('#add-task').click(function(e) {
+//     e.preventDefault();
+//     let data = $('#new-task').serialize();
+//     $.ajax({
+//         type: "post",
+//         url: "/tasks/create",
+//         data: data,
+//         success: function (response) {
+//             $('.main-tile').append(response)
+//             $('.task-row').draggable({
+//                 cursor: 'move',
+//                 opacity: .7,
+//                 revert: 'invalid',
+//                 scroll: true,
+//                 scrollSensitivity: 50,
+//             });
+//         }
+//     });
+// });
 
 $('.task-row').draggable({
     cursor: 'move',
