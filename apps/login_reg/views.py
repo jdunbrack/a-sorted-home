b226c88d43899d2de064f2edf4d2ccbcd17c2ebb
Jordan Dunbrack <jordan.dunbrack@gmail.com>
2019-05-06 19:36:05 -0700

    fixing bug introduced

--- apps/login_reg/views.py
+++ apps/login_reg/views.py
@@ -85,9 +85,9 @@ def age_verify(request):
 def name_verify(request):
     if request.POST['login_reg'] == 'register':
             
-        NAME_REGEX = re.compile(r'^[a-zA-Z]+$')
+        NAME_REGEX = re.compile(r'\d')
 
-        if NAME_REGEX.match(request.POST['first-name']) == None or NAME_REGEX.math(request.POST['last-name']) == None:
+        if NAME_REGEX.match(request.POST['first-name']) != None or NAME_REGEX.math(request.POST['last-name']) != None:
             valid = False
         else:
             valid = True
